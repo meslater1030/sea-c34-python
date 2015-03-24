526f045c761c325122b7b8bdf9c2a1a2efa5bbf5
Megan Slater <megan.e.slater@gmail.com>
2015-03-23 18:56:04 -0700

    debug and add mroe assert statements

--- students/MeganSlater/session02/series.py
+++ students/MeganSlater/session02/series.py
@@ -2,11 +2,14 @@ def fibonacci(n):
     """Define the beginning of the sequence"""
     first = 0
     second = 1
+    output = 0
     """Give a range for the loop"""
     remainder = n - 1
     """Return the appropriate number in the fibonacci sequence"""
     if n == 0:
-        return 0
+        output = first
+    elif n == 1:
+        output = second
     else:
         for i in range(remainder):
             output = first + second
@@ -19,11 +22,14 @@ def lucas(n):
     """Define the beginning of the sequence"""
     first = 2
     second = 1
+    output = 0
     """Give a range for the loop"""
     remainder = n - 1
     """Return the appropriate number in the lucas sequence"""
     if n == 0:
-        return 2
+        output = first
+    elif n == 1:
+        output = second
     else:
         for i in range(remainder):
             output = first + second
@@ -35,9 +41,12 @@ def lucas(n):
 def sum_series(n, first=0, second=1):
     """Give a range for the loop"""
     remainder = n - 1
+    output = 0
     """Return the appropriate number in the sequence"""
     if n == 0:
-        return first
+        output = first
+    elif n == 1:
+        output = second
     else:
         for i in range(remainder):
             output = first + second
@@ -47,6 +56,9 @@ def sum_series(n, first=0, second=1):
 
 if __name__ == "__main__":
     """check to make sure  each function returns the correct answer"""
-    assert fibonacci(7) == 13
-    assert lucas(5) == 8
-    assert sum_series(7) == 13
+    assert(fibonacci(7) == 13)
+    assert(fibonacci(3) == 2)
+    assert(lucas(5) == 11)
+    assert(lucas(1) == 1)
+    assert(sum_series(7) == 13)
+    assert(sum_series(2, 1, 2) == 3)
