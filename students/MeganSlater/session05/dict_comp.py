cfaf36a8f332ee5f87abaf5730f6ecc194059ee5
Megan Slater <megan.e.slater@gmail.com>
2015-04-03 17:59:16 -0700

    All steps complete

--- students/MeganSlater/session05/dict_comp.py
+++ students/MeganSlater/session05/dict_comp.py
@@ -1,5 +1,4 @@
 def step1():
-    # this is for step 1
     food_prefs = {"name": u"Megan",
                           u"city": u"Seattle",
                           u"cake": u"chocolate",
@@ -24,26 +23,46 @@ def step3():
 step3()
 
 
-"""
-def Jimstep5():
-    s2, s3, s4 = [{n for n in range(21) if n % i == 0} for i in range(2, 5)]
+def step4():
+    food_prefs = {"name": u"Megan",
+                          u"city": u"Seattle",
+                          u"cake": u"chocolate",
+                          u"fruit": u"raspberries",
+                          u"salad": u"chef salad",
+                          u"pasta": u"mac and cheese"}
+    food_a = {key: value.count('a') for key, value in food_prefs.iteritems()}
+    print(food_a)
+step4()
+
+
+def step5a():
+    s2 = {x for x in range(21) if x % 2 == 0}
+    s3 = {x for x in range(21) if x % 3 == 0}
+    s4 = {x for x in range(21) if x % 4 == 0}
     print(s2)
     print(s3)
     print(s4)
+step5a()
 
 
-def jonstep5b():
+def step5b():
     sets = s2, s3, s4 = (set(), set(), set())
 
     for i, set_x in zip((2, 3, 4), sets):
         for j in range(21):
             if j % i == 0:
                 set_x.add(j)
-    The rest is Megan's for part c
 
-    [set_x.add(j) for i, set_x in zip((2, 3, 4), sets)
-     for j in range(21) if j % i == 0]
     print(s2)
     print(s3)
     print(s4)
-"""
+step5b()
+
+
+def step5c():
+    s2, s3, s4 = ([{x for x in range(21) if x % i == 0} for i in range(2, 5)])
+    print(s2)
+    print(s3)
+    print(s4)
+step5c()
+
