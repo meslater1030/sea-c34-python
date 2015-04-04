962bcff328f276e0428ff3c04067c54e4d003864
Megan Slater <megan.e.slater@gmail.com>
2015-04-04 15:31:30 -0700

    add class inherit from object class

--- students/MeganSlater/session05/classes.py
+++ students/MeganSlater/session05/classes.py
@@ -3,7 +3,7 @@
 # Can I initalize a class that accpets no arguments?
 
 
-class No_Arguments():
+class No_Arguments(object):
     def __init__(self):
         print("This class has been initialized and has no arguments.")
 No_Arguments()
