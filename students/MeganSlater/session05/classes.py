ef4551328c78e5779341966a4fc1277689d5e15f
Megan Slater <megan.e.slater@gmail.com>
2015-04-04 15:29:21 -0700

    one question asked

--- students/MeganSlater/session05/classes.py
+++ students/MeganSlater/session05/classes.py
@@ -1,1 +1,9 @@
 # One Question
+
+# Can I initalize a class that accpets no arguments?
+
+
+class No_Arguments():
+    def __init__(self):
+        print("This class has been initialized and has no arguments.")
+No_Arguments()
