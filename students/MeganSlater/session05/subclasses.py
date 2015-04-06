245661b14089fc68b11f22af997391e01dc919d7
Megan Slater <megan.e.slater@gmail.com>
2015-04-06 16:52:18 -0700

    update with docstrings

--- students/MeganSlater/session05/subclasses.py
+++ students/MeganSlater/session05/subclasses.py
@@ -1,11 +1,10 @@
 # Four Questions
 
-"""Question 1:
-Can I replace a method that has been inherited from another class?
-"""
-
 
 class Superclass(object):
+    """Question 1:
+    Can I replace a method that has been inherited from another class?
+    """
     def super_method(self):
         print("I'm the method of the super class")
     name = "Super"
@@ -20,20 +19,21 @@ sub_class = Subclass()
 super_class.super_method()
 sub_class.super_method()
 
-"""Question 2:
-Can a subclass inherit from more than one super class?
-"""
-"""
-class Inbred(Superclass, Subclass):
-    def __init__(self):
-        print(self.pizza + self.name)
-this generates an error. Python cannot decide which super_method to use
-not to mention that you wouldn't want to inherit a superclass and its
-subclass.  It would make more sense to make another subclass.
-"""
+# class Inbred(Superclass, Subclass):
+#     """Question 2:
+#     Can a subclass inherit from more than one super class?
+#     """
+#     def __init__(self):
+#         print(self.pizza + self.name)
+# this generates an error. Python cannot decide which super_method to use
+# not to mention that you wouldn't want to inherit a superclass and its
+# subclass.  It would make more sense to make another subclass.
 
 
 class Other_Superclass(object):
+    """Question 2:
+    Can a subclass inherit from more than one super class?
+    """
     pizza = "Cheese"
 
 
@@ -42,13 +42,12 @@ class Inbred_Two(Superclass, Other_Superclass):
         print(Superclass.name + Other_Superclass.pizza)
 inbred_class = Inbred_Two()
 
-"""Question 3:
-Can a method in a subclass alter a method in a superclass without
-replacing it entirely?
-"""
-
 
 class Extender(Superclass):
+    """Question 3:
+    Can a method in a subclass alter a method in a superclass without
+    replacing it entirely?
+    """
     def super_method(self):
         Superclass.super_method(self)
         print("I'm an extension of the super method")
