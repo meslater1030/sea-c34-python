aadc569a99b7a529fa29322626f55466a709556e
Megan Slater <megan.e.slater@gmail.com>
2015-04-06 16:43:35 -0700

    add docstrings

--- students/MeganSlater/session05/lambda.py
+++ students/MeganSlater/session05/lambda.py
@@ -1,11 +1,17 @@
 def lambda_loop(n, x):
+    """Function returns list of n function such that each one, when called
+    will return the input value, incremented by an increasing number using a
+    for loop.
+    """
     lambda_list = []
     for i in range(n):
         lambda_list.append(lambda x, e=i: x + e)
     for function in lambda_list:
         print(function(x))
 lambda_loop(4, 5)
+
 """
+notes for Megan:
 e is a keyword variable.  We need
 to set a keyword variable so that they're unique for
 every function that's generated.
@@ -19,6 +25,10 @@ for every instance in in some iterable sequence]
 
 
 def lambda_constructor(n, x):
+    """Function returns list of n function such that each one, when called
+    will return the input value, incremented by an increasing number using a
+    list consructor.
+    """
     lambda_list = [lambda x, e=i: x + e for i in range(n)]
     for function in lambda_list:
         print(function(x))
