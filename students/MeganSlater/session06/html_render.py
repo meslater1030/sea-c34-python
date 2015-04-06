7b80f3b1afb8a28ff6f2e6af428440b9e7a41c13
Megan Slater <megan.e.slater@gmail.com>
2015-04-05 20:13:21 -0700

    finished part1

--- students/MeganSlater/session06/html_render.py
+++ students/MeganSlater/session06/html_render.py
@@ -1,23 +1,26 @@
 class Element(object):
-    def __init__(self, ind=u""):
-        self.ind = ind
-        self.html_list = [u"<!DOCTYPE html>\n", u"<html>\n", u"</html>\n"]
+    indent = ""
+    tag_name = "html"
+
+    def __init__(self):
+        self.lines = [u"<!DOCTYPE html>\n", u"<" + self.tag_name + ">\n",
+                      u"</" + self.tag_name + ">\n"]
 
     def append(self, words):
         self.words = words
-        self.html_list.pop()
-        self.html_list.extend(self.words)
-        self.html_list.append(u"\n</html>\n")
+        self.lines.pop()
+        self.lines.extend(self.words)
+        self.lines.append(u"\n</" + self.tag_name + ">\n")
 
     def render(self, file_out):
         self.file_out = file_out
-        [self.file_out.write(item) for item in self.html_list]
+        [self.file_out.write(item) for item in self.lines]
 
 
 class Html(object):
     def __init__(self, ind=""):
         self.ind = ind
-        self.lines = ["<!DOCTYPE html>\n", "<html>\n", "</html>\n"]
+        self.lines = [u"<!DOCTYPE html>\n", u"<html>\n", u"</html>\n"]
 
     def append(self, tag):
         self.tag = tag
@@ -37,9 +40,9 @@ class Html(object):
 
 
 class Body(object):
-    def __init__(self, ind="    "):
+    def __init__(self, ind=u"    "):
         self.ind = ind
-        self.lines = [self.ind + "<body>\n", self.ind + "</body>\n"]
+        self.lines = [self.ind + u"<body>\n", self.ind + u"</body>\n"]
 
     def append(self, tag):
         self.tag = tag
@@ -56,10 +59,11 @@ class Body(object):
                 interim_list.append(inner_list)
         self.lines = interim_list
 
+
 class Head(object):
     def __init__(self, ind="    "):
         self.ind = ind
-        self.lines = [self.ind + "<head>\n", self.ind + "</head>\n"]
+        self.lines = [self.ind + u"<head>\n", self.ind + u"</head>\n"]
 
     def append(self, tag):
         self.tag = tag
@@ -69,26 +73,26 @@ class Head(object):
 class Title(object):
     def __init__(self, text, ind="        "):
         self.text = text
-        self.output = '%s <title>%s</title>\n' % (ind, self.text)
+        self.output = u'%s <title>%s</title>\n' % (ind, self.text)
 
 
 class Meta(object):
     def __init__(self, charset="", ind="        "):
         self.charset = charset
-        self.output = '%s <meta charset="%s"/>\n' % (ind, self.charset)
+        self.output = u'%s <meta charset="%s"/>\n' % (ind, self.charset)
 
 
 class P(object):
     def __init__(self, text, style="", ind="        "):
         self.text = text
         self.style = style
-        self.output = '%s<p style="%s">%s</p>\n' % (ind, self.style, self.text)
+        self.output = u'%s<p style="%s">%s</p>\n' % (ind, self.style, self.text)
 
 
 class Hr():
     def __init__(self, ind="        "):
         self.ind = ind
-        self.output = self.ind + "<hr />\n"
+        self.output = self.ind + u"<hr />\n"
 
 
 class A(object):
@@ -96,15 +100,15 @@ class A(object):
         self.ind = ind
         self.url = url
         self.text = text
-        self.output = '%s<a href="%s">%s</a> ' % (ind, self.url, self.text)
+        self.output = u'%s<a href="%s">%s</a> ' % (ind, self.url, self.text)
 
 
 class H(object):
     def __init__(self, num, text, ind="        "):
         self.num = num
         self.text = text
-        self.output = '%s<h%s>%s</h%s>\n' % (ind, self.num,
-                                             self.text, self.num)
+        self.output = u'%s<h%s>%s</h%s>\n' % (ind, self.num,
+                                              self.text, self.num)
 
 
 class Ul(object):
@@ -112,8 +116,8 @@ class Ul(object):
         self.id = id
         self.style = style
         self.ind = ind
-        self.output = [self.ind + "<ul id='" + self.id + "'>\n", self.ind +
-                       "</ul>\n"]
+        self.output = [self.ind + u"<ul id='" + self.id + u"'>\n", self.ind +
+                       u"</ul>\n"]
 
     def append(self, tag):
         self.tag = tag
@@ -133,8 +137,8 @@ class Li(object):
         self.text = text
         self.style = style
         self.ind = ind
-        self.output = [self.ind + "<li style='" + self.style + "'>",
-                       self.text, "</li>\n"]
+        self.output = [self.ind + u"<li style='" + self.style + "'>",
+                       self.text, u"</li>\n"]
 
     def append(self, text):
         self.text = text
