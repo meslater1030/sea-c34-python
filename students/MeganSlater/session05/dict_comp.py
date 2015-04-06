5152e7804245a1792542562ec5a96ea16473dd9f
Megan Slater <megan.e.slater@gmail.com>
2015-04-06 16:33:09 -0700

    add docstrings

--- students/MeganSlater/session05/dict_comp.py
+++ students/MeganSlater/session05/dict_comp.py
@@ -1,4 +1,5 @@
 def step1():
+    """print string from dictionary"""
     food_prefs = {"name": u"Megan",
                           u"city": u"Seattle",
                           u"cake": u"chocolate",
@@ -12,6 +13,8 @@ step1()
 
 
 def step2():
+    """build dictionary that matches hex values with the appropriate base 10 value
+     with list comprehension"""
     print(dict(zip(range(16),
           map(lambda x: hex(x), range(16)))))
 step2()
@@ -24,6 +27,8 @@ step3()
 
 
 def step4():
+    """build dictionary that matches the key of the existing dictionary with
+    the number of a's in the value of the old dictionary"""
     food_prefs = {"name": u"Megan",
                           u"city": u"Seattle",
                           u"cake": u"chocolate",
@@ -36,6 +41,9 @@ step4()
 
 
 def step5a():
+    """build three separate sets using three separate dictionary comprehensions
+    that yields all numbers between 0 and 20 divisible by a particular number.
+    """
     s2 = {x for x in range(21) if x % 2 == 0}
     s3 = {x for x in range(21) if x % 3 == 0}
     s4 = {x for x in range(21) if x % 4 == 0}
@@ -46,6 +54,9 @@ step5a()
 
 
 def step5b():
+    """build three separate sets using one for loop
+    that yields all numbers between 0 and 20 divisible by a particular number
+    """
     sets = s2, s3, s4 = (set(), set(), set())
 
     for i, set_x in zip((2, 3, 4), sets):
@@ -60,6 +71,9 @@ step5b()
 
 
 def step5c():
+    """build three separate sets using one set comprehension
+    that yields all numbers between 0 and 20 divisible by a particular number
+    """
     s2, s3, s4 = ([{x for x in range(21) if x % i == 0} for i in range(2, 5)])
     print(s2)
     print(s3)
