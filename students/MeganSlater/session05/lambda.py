578ec9fa413f37c87905e409f5210ba58ddda919
Megan Slater <megan.e.slater@gmail.com>
2015-04-07 18:05:48 -0700

    changed function input

--- students/MeganSlater/session05/lambda.py
+++ students/MeganSlater/session05/lambda.py
@@ -1,4 +1,4 @@
-def lambda_loop(n, x):
+def function_builder(n):
     """Function returns list of n function such that each one, when called
     will return the input value, incremented by an increasing number using a
     for loop.
@@ -6,9 +6,10 @@ def lambda_loop(n, x):
     lambda_list = []
     for i in range(n):
         lambda_list.append(lambda x, e=i: x + e)
-    for function in lambda_list:
-        print(function(x))
-lambda_loop(4, 5)
+    return lambda_list
+
+the_list = function_builder(20)[2](3)
+print the_list
 
 """
 notes for Megan:
@@ -24,12 +25,13 @@ for every instance in in some iterable sequence]
 """
 
 
-def lambda_constructor(n, x):
+def lambda_constructor(n):
     """Function returns list of n function such that each one, when called
     will return the input value, incremented by an increasing number using a
     list consructor.
     """
-    lambda_list = [lambda x, e=i: x + e for i in range(n)]
-    for function in lambda_list:
-        print(function(x))
-lambda_constructor(4, 5)
+    return [lambda x, e=i: x + e for i in range(n)]
+
+the_list = function_builder(20)[2](3)
+print the_list
+
